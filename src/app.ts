bd4d5f781206f65b77cda46d559ef70b4bb13cb7
Naeem Islam <naeemislam.hasan74@gmail.com>
2026-01-30 11:21:19 +0600

    reviews correct in app.ts

--- src/app.ts
+++ src/app.ts
@@ -5,10 +5,10 @@ import cors from 'cors';
 import { tutorRouter } from "./modules/tutor/profileManagement/tutor.router";
 import { availabilityRouter } from "./modules/tutor/availabiltyManagement/management.router";  
 import { studentRouter } from "./modules/student/student.router";
-import { categoryRouter } from "./modules/tutor/categoryMng/category.router";
-import { reviewRouter } from "./modules/tutor/review/review.router";
+import { categoryRouter } from "./modules/tutor/categoryMng/category.router"; 
 import { dashboardRouter } from "./modules/tutor/dashboard/dash.router";
-  
+import { publicTutorRouter } from "./modules/tutor/public/public.router"; 
+import { reviewRouter } from "./modules/tutor/review/review.router";
 
 const app: Application = express();
 
@@ -21,21 +21,21 @@ app.use(express.json());
 
 app.all("/api/auth/*splat", toNodeHandler(auth));
 
- // Tutor routes
+// Tutor routes
 app.use("/api/tutors", tutorRouter);
 app.use("/api/tutors/availability", availabilityRouter);
 app.use("/api/tutors/categories", categoryRouter);
-
 app.use("/api/tutors/dashboard", dashboardRouter);
-
-app.use("/api/students", studentRouter);
-
-
 app.use("/api/tutors/reviews", reviewRouter);
+app.use("/api/tutors/public", publicTutorRouter);  
+
+// Student routes
+app.use("/api/students", studentRouter);
 
 app.get("/", (req, res) => {
     res.send("Hello, World!");
 });
+
 // app.use(notFound)
 // app.use(errorHandler)
 
